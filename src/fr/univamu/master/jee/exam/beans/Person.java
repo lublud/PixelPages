fd329f86e1ac938c301ad0ac0ca0fa8227bb97f1
lublud <luido992@gmail.com>
2014-12-08 23:45:31 +0100

    getter/setter and constructor added to Person.java

--- src/fr/univamu/master/jee/exam/beans/Person.java
+++ src/fr/univamu/master/jee/exam/beans/Person.java
@@ -1,5 +1,87 @@
 package fr.univamu.master.jee.exam.beans;
 
-public class Person {
+import java.util.Date;
 
+public class Person {
+	private int idPerson;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String website;
+	private Date birthdate;
+	private String password;
+	
+	
+	public Person() {
+		super();
+	} // Person()
+
+
+	public int getIdPerson() {
+		return idPerson;
+	} // getIdPerson()
+
+
+	public void setIdPerson(int idPerson) {
+		this.idPerson = idPerson;
+	} // setIdPerson()
+
+
+	public String getFirstName() {
+		return firstName;
+	} // getFirstName()
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	} // setFirstName()
+
+
+	public String getLastName() {
+		return lastName;
+	} // getLastName()
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	} // setLastName()
+
+
+	public String getEmail() {
+		return email;
+	} // getEmail()
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	} // setEmail()
+
+
+	public String getWebsite() {
+		return website;
+	} // getWebsite()
+
+
+	public void setWebsite(String website) {
+		this.website = website;
+	} // setWebsite()
+
+
+	public Date getBirthdate() {
+		return birthdate;
+	} // getBirthdate()
+
+
+	public void setBirthdate(Date birthdate) {
+		this.birthdate = birthdate;
+	} // setBirthdate()
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	} // setPassword()
+	
+	
+	
+	
 }
