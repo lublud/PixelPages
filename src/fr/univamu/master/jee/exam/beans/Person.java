3ae1068e51ef47b91c684b4335e08327d40a369d
lublud <luido992@gmail.com>
2014-12-09 11:57:31 +0100

    Revert "persistence.xml added and Person.java modified to use JPA"
    This reverts commit 64b08988201eb836d961c97546dba5eb15d6f187.

--- src/fr/univamu/master/jee/exam/beans/Person.java
+++ src/fr/univamu/master/jee/exam/beans/Person.java
@@ -1,104 +1,87 @@
 package fr.univamu.master.jee.exam.beans;
 
-import java.io.Serializable;
 import java.util.Date;
 
-import javax.persistence.Basic;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-
-@Entity(name = "Person")
-public class Person implements Serializable {
-	private static final long serialVersionUID = 1L;
-
-	@Id()
-	@GeneratedValue(strategy = GenerationType.AUTO)
+public class Person {
 	private int idPerson;
-
-	@Basic(optional = false)
-	@Column(name = "firstName", length = 64, nullable = false)
 	private String firstName;
-	
-	@Basic(optional = false)
-	@Column(name = "lastName", length = 26400, nullable = false)
 	private String lastName;
-	
-	@Basic(optional = true)
-	@Column(name = "email", length = 128, nullable = false)
 	private String email;
-	
-	@Basic(optional = true)
-	@Column(name = "website", length = 128)
 	private String website;
-	
-	@Basic()
-	@Temporal(TemporalType.DATE)
-	@Column(name = "birthdate", nullable = false)
 	private Date birthdate;
-	
-	@Basic(optional = false)
-	@Column(name = "password", length = 256, nullable = false)
 	private String password;
-
+	
+	
 	public Person() {
 		super();
 	} // Person()
+
 
 	public int getIdPerson() {
 		return idPerson;
 	} // getIdPerson()
 
+
 	public void setIdPerson(int idPerson) {
 		this.idPerson = idPerson;
 	} // setIdPerson()
+
 
 	public String getFirstName() {
 		return firstName;
 	} // getFirstName()
 
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	} // setFirstName()
+
 
 	public String getLastName() {
 		return lastName;
 	} // getLastName()
 
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	} // setLastName()
+
 
 	public String getEmail() {
 		return email;
 	} // getEmail()
 
+
 	public void setEmail(String email) {
 		this.email = email;
 	} // setEmail()
+
 
 	public String getWebsite() {
 		return website;
 	} // getWebsite()
 
+
 	public void setWebsite(String website) {
 		this.website = website;
 	} // setWebsite()
+
 
 	public Date getBirthdate() {
 		return birthdate;
 	} // getBirthdate()
 
+
 	public void setBirthdate(Date birthdate) {
 		this.birthdate = birthdate;
 	} // setBirthdate()
 
+
 	public void setPassword(String password) {
 		this.password = password;
 	} // setPassword()
-
+	
+	
+	
+	
 }
